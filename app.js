a2f42d8827f7af79596de5164100185b911ba910
Erik Noonan <eriknoonan@Eriks-MacBook-Pro.local>
2017-01-06 13:01:42 -0600

    edited formatting slightly

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function Student(firstName, favoriteColor) {
-  this.firstName = firstName;
-  this.favoriteColor = favoriteColor;
+    this.firstName = firstName;
+    this.favoriteColor = favoriteColor;
 }
 
 /**
@@ -10,32 +10,32 @@ function Student(firstName, favoriteColor) {
 
 var erik = new Student('Erik', 'Blue');
 
-var charlie = new Student ('Charlie', 'Green');
+var charlie = new Student('Charlie', 'Green');
 
 
 
 /**
-  * 2. Create a function that takes one argument. That argument will
-  * represent a single student object. The function will log the student's
-  * name.
-  */
-function userStudentName(special){
-  console.log(special.firstName);
+ * 2. Create a function that takes one argument. That argument will
+ * represent a single student object. The function will log the student's
+ * name.
+ */
+function userStudentName(special) {
+    console.log(special.firstName);
 }
 
 /***** SWITCH PROGRAMMERS *****/
 
 /**
-  * 3. Create a function that takes one argument. That argument will
-  * represent a single student object. The function will log the student's
-  * favoriteColor.
-  */
-function userStudentColor(special){
-  console.log(special.favoriteColor);
+ * 3. Create a function that takes one argument. That argument will
+ * represent a single student object. The function will log the student's
+ * favoriteColor.
+ */
+function userStudentColor(special) {
+    console.log(special.favoriteColor);
 }
 /**
-  * 4. Put both students created in Step 1 into an array called students.
-  */
+ * 4. Put both students created in Step 1 into an array called students.
+ */
 var students = [erik, charlie];
 /***** SWITCH PROGRAMMERS *****/
 
@@ -43,8 +43,8 @@ var students = [erik, charlie];
  * 5. Loop over the students array and invoke both functions from Step 2 and
  * Step 3. You should see console logs for each students firstName and favoriteColor.
  */
-for (var i=0; i<students.length; i++){
-  // console.log("student object : "+students[i]);
-  userStudentName(students[i]);
-  userStudentColor(students[i]);
+for (var i = 0; i < students.length; i++) {
+    // console.log("student object : "+students[i]);
+    userStudentName(students[i]);
+    userStudentColor(students[i]);
 }
